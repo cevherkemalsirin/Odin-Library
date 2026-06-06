83c30d22212ea0f7f59a77acc60a297376616ac8
Cevher Kemal Sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2026-06-06 15:13:08 +0300

    Finish library project mvp

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
-const formAddBtn = document.querySelector(".FormAddBtn");
+const form = document.querySelector(".dialogForm");
+const container = document.querySelector(".bookContainer");
 
 function Book(title, author, pages, read) {
-    this.title = title;
+    if(!new.target)
+    {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.id = -1;
 }
 
-const myLibrary = [];
+Book.prototype.toggleRead = function(){
+    this.read = !this.read;
+}
+
+const myLibraryMap = new Map();
 
 function AddToTheLibrary(title,author,pages,read)
 {
     const book = new Book(title,author,pages,read);
     book.id = crypto.randomUUID();
-    myLibrary.push(book);
+    myLibraryMap.set(book.id, book);
+    AddBooksToContainer(book);
 }
 
-function DisplayBooks()
+
+function AddBooksToContainer(book)
 {
-    for(const book of myLibrary)
-    {
-        console.log(book);
-    }
+    
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+    bookCard.setAttribute("data-id", book.id);
+    const title = document.createElement("h2");
+    title.textContent = book.title;
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book.author}`;
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${book.pages}`;
+    const readBtn = document.createElement("button");
+    readBtn.textContent = book.read ? "Read" : "Not Read";
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    bookCard.append(title,author,pages,readBtn,removeBtn);
+    container.appendChild(bookCard);
+
+    removeBtn.addEventListener("click", function(){
+        myLibraryMap.delete(book.id);
+        container.removeChild(bookCard);
+    });
+
+    readBtn.addEventListener("click", function(){
+        book.toggleRead();
+        readBtn.textContent = book.read ? "Read" : "Not Read";
+    });
 }
 
-formAddBtn.addEventListener("click", function(e){
+form.addEventListener("submit", function(e){
     e.preventDefault();
+    
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
     const read = document.querySelector("#read").checked;
-    document.querySelector("#addBookDialog").close();
     AddToTheLibrary(title,author,pages,read);
-    DisplayBooks();
+    form.reset();
+    document.querySelector("#addBookDialog").close();
 });
 
